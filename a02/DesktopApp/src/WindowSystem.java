05e0e1a27ce8c637c7484317b3a7177f9c3f7406
Vitalii Isaenko <lookyisaenko@gmail.com>
2018-04-24 09:33:50 +0200

    Add simple window and simple window set to windows system

--- a02/DesktopApp/src/WindowSystem.java
+++ a02/DesktopApp/src/WindowSystem.java
@@ -1,10 +1,20 @@
 import de.rwth.hci.Graphics.GraphicsEventSystem;
 
 import java.awt.*;
+import java.util.Set;
 
 public class WindowSystem extends GraphicsEventSystem {
+    private Set<SimpleWindow> simpleWindows;
     private int width;
     private int height;
+
+    public void addSimpleWindow(SimpleWindow simpleWindow){
+        simpleWindows.add(simpleWindow);
+    }
+
+    public void removeSimpleWindow(SimpleWindow simpleWindow) {
+        simpleWindows.remove(simpleWindow);
+    }
 
     public WindowSystem(int width, int height) {
         super(width, height);
@@ -24,7 +34,7 @@ public class WindowSystem extends GraphicsEventSystem {
 
     protected void handlePaint() {
         setColor(new Color(255, 122, 36));
-//        drawLine(30, 70, 60, 150);
+        drawLine(30, 70, 60, 150);
         drawLine(0.1f, 0.2f, 0.7f, 0.8f);
     }
 }
