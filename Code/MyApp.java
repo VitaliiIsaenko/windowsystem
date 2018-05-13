534c46ea0239e91d84b0a15190c7ea683e156b23
Anastasiia <anastasiia.belova@rwth-aachen.de>
2018-05-13 18:04:34 +0200

    Adding new MyApp file

--- Code/MyApp.java
+++ Code/MyApp.java
@@ -1,3 +1,12 @@
+package windowsystem;
+
+import windowsystem.coordinates.Coordinates;
+import windowsystem.coordinates.Point;
+import windowsystem.rat.RATButton;
+import windowsystem.rat.RATLabel;
+import windowsystem.rat.RATTextField;
+import windowsystem.rat.RATWidget;
+
 import java.awt.*;
 
 /**
@@ -6,31 +15,37 @@ import java.awt.*;
 public class MyApp {
 
     public static void main(String[] args) {
-        WindowSystem ws = new WindowSystem(600, 600, Color.PINK);
+        WindowSystem ws = new WindowSystem(600, 600, Color.YELLOW);
         ws.setWindowManager(new WindowManager(ws));
 
-        SimpleWindow window1 = ws.addWindow(250, 250, "Window 1");
-        SimpleWindow window2 = ws.addWindow(250, 250, "Window 2");
-        SimpleWindow window3 = ws.addWindow(250, 250, "Window 3");
+        SimpleWindow window1 = ws.addWindow(300, 300, "Window 1");
 
-        RATButton button = new RATButton(new Point(5,5), "Click!", Color.LIGHT_GRAY, Color.WHITE, Color.BLACK);
-        RATButton button2 = new RATButton(new Point(5, 35), "Hello", Color.LIGHT_GRAY, Color.WHITE, Color.BLACK);
-        String myString = "Hi, guys!";
-        RATTextField textField = new RATTextField(new Point(5, 75),myString,Color.WHITE, Color.CYAN, Color.BLACK);
+        RATButton button = new RATButton(new Point(125,20), "Deutsch", Color.LIGHT_GRAY, Color.WHITE, Color.BLACK);
+        RATButton button2 = new RATButton(new Point(125, 65), "English", Color.LIGHT_GRAY, Color.WHITE, Color.BLACK);
+        RATButton button3 = new RATButton(new Point(125, 105), "Francais", Color.LIGHT_GRAY, Color.WHITE, Color.BLACK);
+        RATButton button4 = new RATButton(new Point(230, 255), "Cancel", Color.LIGHT_GRAY, Color.WHITE, Color.BLACK);
+        RATLabel label = new RATLabel(new Point(20,175), "No button pressed", Color.WHITE);
 
         button.addActionListener((ae) -> {
-            textField.setText("Peeeep");
+            label.setText("Guten Tag!");
         });
+
+        button2.addActionListener((ae)-> {
+            label.setText("Hi there!");
+        });
+
+        button3.addActionListener((ae)-> {
+            label.setText("Bonjour!");
+        });
+
+        button4.addActionListener((ae -> {
+            window1.getWindowSystem().removeWindow(window1.getId());}));
 
         window1.addWidget(button);
         window1.addWidget(button2);
-        window1.addWidget(textField);
+        window1.addWidget(button3);
+        window1.addWidget(button4);
+        window1.addWidget(label);
 
-        RATButton button3 = new RATButton(new Point(5,5), "Click!", Color.LIGHT_GRAY, Color.WHITE, Color.BLACK);
-
-        window2.addWidget(button3);
-
-        RATLabel label = new RATLabel(new Point(5,5), "Just Label", Color.GREEN);
-        window3.addWidget(label);
     }
 }
